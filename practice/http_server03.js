7c2d1a22934bc8b1219770c72f463263b5846df7
Shinder Lin <lsd0125@gmail.com>
2022-01-25 15:16:12 +0800

    readFile

--- practice/http_server03.js
+++ practice/http_server03.js
@@ -3,15 +3,16 @@ const fs = require('fs');
 
 
 const server = http.createServer((req, res)=>{
-    res.writeHead(200, {
-        'Content-Type': 'text/html; charset=utf-8'
-    });
-    fs.writeFile(__dirname + '/headers.txt', JSON.stringify(req.headers, null, 4), error => {
+
+    fs.readFile(__dirname + '/headers.txt', (error, data) => {
         if(error){
             console.log(error);
             res.end('錯誤');
         } else {
-            res.end('沒問題');
+            res.writeHead(200, {
+                'Content-Type': 'application/json; charset=utf-8'
+            });
+            res.end(data);
         }
     });
 
