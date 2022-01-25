6dd53662e210ae0f7975147176223496af18a491
Shinder Lin <lsd0125@gmail.com>
2022-01-25 10:36:10 +0800

    class

---
+++ practice/person.js
@@ -0,0 +1,29 @@
+
+class Person {
+    
+    constructor(name='noname', age=0) {
+        this.name = name;
+        this.age = age;
+    }
+
+    toJSON(){
+        return {
+            name: this.name,
+            age: this.age,
+        }
+    }
+
+    sayHello(){
+        return `Hello ${this.name}`;
+    }
+}
+
+const p1 = new Person('Bill', 23);
+
+
+console.log(p1.sayHello());
+console.log(JSON.stringify(p1.toJSON()));
+console.log(JSON.stringify(p1));
+
+
+
