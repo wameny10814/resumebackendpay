0d98d6164308636bbeec143261eb594e041c3171
Shinder Lin <lsd0125@gmail.com>
2022-01-26 10:08:43 +0800

    express begin

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res)=>{
+    res.send('<h2>Hello</h2>');
+});
+
+app.listen(3000, ()=>{
+    console.log('server started');
+});
