51f55b0738966b22381aa871cb5db3929aab3c98
Shinder Lin <lsd0125@gmail.com>
2022-01-26 10:29:09 +0800

    使用 .env

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const express = require('express');
 
 const app = express();
@@ -6,6 +7,7 @@ app.get('/', (req, res)=>{
     res.send('<h2>Hello</h2>');
 });
 
-app.listen(3000, ()=>{
-    console.log('server started');
+const port = process.env.PORT || 3001;
+app.listen(port, ()=>{
+    console.log(`server started: ${port} - `, new Date());
 });
