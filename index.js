f7ec3c67f45c9e2408a64b00c9895b934de3d527
Shinder Lin <lsd0125@gmail.com>
2022-01-27 11:55:47 +0800

    top-level middleware

--- index.js
+++ index.js
@@ -11,7 +11,12 @@ app.get('/a.html', (req, res)=>{
     res.send(`<h2>動態內容</h2><p>${Math.random()}</p>`);
 });
 */
+
+// Top-level middleware
+app.use(express.urlencoded({extended: false}));
 app.use(express.static('public'));
+
+
 
 app.get('/', (req, res)=>{
     res.render('home', {name:'Shinder'});
@@ -37,8 +42,8 @@ app.get('/try-qs', (req, res)=>{
     res.json(req.query);
 });
 
-const urlencodedParser = express.urlencoded({extended: false});
-app.post('/try-post', urlencodedParser, (req, res)=>{
+
+app.post('/try-post', (req, res)=>{
     res.json(req.body);
 });
 
