8eef3cb432a17715e2ff24085f2ef9c356fb8e0d
Shinder Lin <lsd0125@gmail.com>
2022-02-09 10:38:26 +0800

    search  1

--- routes/address-book.js
+++ routes/address-book.js
@@ -11,6 +11,13 @@ async function getListData(req, res){
         return res.redirect('/address-book/list');
     }
 
+    let search = req.query.search ? req.query.search : '';
+    search = search.trim(); // 去掉頭尾空白
+    let sqlWhere = ' WHERE 1 ';
+    if(search){
+        sqlWhere = ` AND \`name\` LIKE ${db.escape('%'+search+'%')} `;
+    }
+
     // 輸出
     const output = {
         // success: false,
@@ -21,7 +28,8 @@ async function getListData(req, res){
         rows: []
     };
 
-    const t_sql = "SELECT COUNT(1) num FROM address_book";
+    const t_sql = `SELECT COUNT(1) num FROM address_book ${sqlWhere} `;
+    // return res.send(t_sql); // 除錯用
     const [rs1] = await db.query(t_sql);
     const totalRows = rs1[0].num;
     // let totalPages = 0;
@@ -33,7 +41,7 @@ async function getListData(req, res){
             return res.redirect(`/address-book/list?page=${output.totalPages}`);
         }
 
-        const sql = `SELECT * FROM \`address_book\` LIMIT ${perPage*(page-1)}, ${perPage} `;
+        const sql = `SELECT * FROM \`address_book\` ${sqlWhere} ORDER BY sid DESC LIMIT ${perPage*(page-1)}, ${perPage} `;
         const [rs2] = await db.query(sql);
         rs2.forEach(el=>{
             el.birthday = res.locals.toDateString(el.birthday);
