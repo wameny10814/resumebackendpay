498b3d6f1900d5d3e3965b605a1e698aaf0fe13b
Shinder Lin <shinder.lin@gmail.com>
2022-03-07 10:51:37 +0800

    表單呈現資料

--- react-try/Config.js
+++ react-try/Config.js
@@ -1,6 +1,8 @@
 const host = 'http://localhost:3500';
 const config = {
-    AB_LIST: `${host}/address-book/api/list`
+    AB_LIST: `${host}/address-book/api/list`,
+    MYFORM_API: `${host}/admin2/myform`,
+    IMG_PATH: `${host}/img`,
 
 };
 export default config;
--- react-try/MyForm.js
+++ react-try/MyForm.js
@@ -4,12 +4,16 @@ import config from './Config';
 import { useHistory, useLocation } from 'react-router-dom';
 
 function MyForm() {
+  const [row, setRow] = useState({});
  
   useEffect(()=>{
-
+      (async ()=>{
+        const obj = await (await fetch(config.MYFORM_API + `/1`)).json();
+        console.log(obj);
+        setRow(obj[0]);
+      })();
   }, []);
-
- 
+  console.log(row);
 
   return (
     <div className="App">
@@ -17,17 +21,19 @@ function MyForm() {
         <div className="row"><div className="col-lg-6">
             <form>
                 <div className="mb-3">
-                    <label for="exampleInputEmail1" className="form-label">Email address</label>
-                    <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"/>
-                    <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
+                    <label htmlFor="account" className="form-label">account</label>
+                    <input type="text" className="form-control" disabled
+                        value={row.account || ''}
+                    />
                 </div>
                 <div className="mb-3">
-                    <label for="exampleInputPassword1" className="form-label">Password</label>
-                    <input type="password" className="form-control" id="exampleInputPassword1"/>
+                    <label htmlFor="nickname" className="form-label">nickname</label>
+                    <input type="text" className="form-control" id="nickname" name="nickname" value={row.nickname}/>
                 </div>
+
                 <div className="mb-3 form-check">
-                    <input type="checkbox" className="form-check-input" id="exampleCheck1"/>
-                    <label className="form-check-label" for="exampleCheck1">Check me out</label>
+                    <input type="file" className="form-check-input" id="avatar" name="avatar"/>
+                    <img src={config.IMG_PATH + '/' + (row.avatar || 'default.png')} alt=""/>
                 </div>
                 <button type="submit" className="btn btn-primary">Submit</button>
             </form>
