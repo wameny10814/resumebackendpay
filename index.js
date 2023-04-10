60a3d692981b9603b9b7eaf6d71490c0f57f734f
yuchen <wameny10814@gmail.com>
2023-04-10 14:48:01 +0800

    230410

--- index.js
+++ index.js
@@ -20,11 +20,7 @@ const jwt = require('jsonwebtoken');
 const app = express();
 
 app.set('view engine', 'ejs');
-/*
-app.get('/a.html', (req, res)=>{
-    res.send(`<h2>動態內容</h2><p>${Math.random()}</p>`);
-});
-*/
+
 
 // Top-level middleware
 const corsOptions = {
@@ -40,222 +36,19 @@ app.use(express.json()); // application/json
 app.use(express.static('public'));
 app.use('/joi', express.static('node_modules/joi/dist/'));
 
-app.use(session({
-    saveUninitialized: false,
-    resave: false,
-    secret: 'sdkfkdh984576894kjdkgjhdfkkjdfgjkfjsdfjhskAAAkdfjdsf',
-    store: sessionStore,
-    cookie: {
-        maxAge: 1200000
-    }
-}));
-// 自訂的 頂層 middleware
-app.use((req, res, next)=>{
-    res.locals.shin = '哈囉';
-    // res.send('oooo'); // 回應之後, 不會往下個路由規則
-
-    // template helper functions 樣版輔助函式
-    res.locals.toDateString = d => moment(d).format('YYYY-MM-DD');
-    res.locals.toDatetimeString = d => moment(d).format('YYYY-MM-DD HH:mm:ss');
-
-
-    // 如果有 token 就解析(驗證)完放在 res.locals.auth
-    res.locals.auth = null; // 自訂的變數, 設定有沒有身份驗證, 預設值為 null
-    let auth = req.get('Authorization');
-    if(auth && auth.indexOf('Bearer ')===0){
-        auth = auth.slice(7);
-        try{
-            const payload = jwt.verify(auth, process.env.JWT_KEY);
-            res.locals.auth = payload;
-        } catch(ex){}
-    }
-
-
-
-
-    next();
-});
-
-app.get('/', (req, res)=>{
-    res.render('home', {name:'Shinder'});
-});
-
-app.get('/a/b', (req, res)=>{
-    res.render('home', {name:'Shinder'});
-});
-
-
-app.get('/json-sales', (req, res)=>{
-    // req.query.orderByCol=age
-    // req.query.orderByRule=desc
-
-    const sales = require('./data/sales');  // 進來變成陣列
-    // TODO: 排序
-    console.log(sales);
-    res.render('json-sales', {sales});
-    
-});
-
-app.get('/try-qs', (req, res)=>{
-    res.json(req.query);
-});
-
-
-app.post('/try-post', (req, res)=>{
-    res.json(req.body);
-});
-
-app.get('/try-post-form', (req, res)=>{
-    res.render('try-post-form');
-});
-app.post('/try-post-form', (req, res)=>{
-    res.render('try-post-form', req.body);
-});
-
-app.post('/try-upload', upload.single('avatar'), async (req, res)=>{
-    res.json(req.file);
-    /*
-    const types = ['image/jpeg', 'image/png'];
-    const f = req.file;
-    if(f && f.originalname){
-        if(types.includes(f.mimetype)){
-            await fs.rename(f.path, __dirname + '/public/img/' + f.originalname);
-            return res.redirect('/img/' + f.originalname);
-        } else {
-            return res.send('檔案類型不符');
-        }
-    }
-    res.send('bad');
-    */
-});
-
-app.post('/try-uploads', upload.array('photos'), async (req, res)=>{
-    const result = req.files.map(({mimetype, filename, size}) => {
-        return {mimetype, filename, size};
-    });
-
-    /*
-    const result = req.files.map(el => {
-        return {
-            "mimetype": el.mimetype,
-            "filename": el.filename,
-            "size": el.size
-        }
-    });
-*/
-    res.json(result);
-});
-
-app.get('/aa', (req, res)=>{
-    // 錯誤的作法
-    res.send('aaa');
-    res.send('bbb');
-});
-
-app.get('/my-params1/:action?/:id?', (req, res)=>{
-    res.json(req.params);
-});
-
-app.get(['/xxx', '/yyy'], (req, res)=>{
-    res.json({x:'y', url: req.url});
-});
-
-app.get(/^\/m\/09\d{2}-?\d{3}-?\d{3}$/i, (req, res)=>{
-    let u = req.url.split('?')[0];
-    u = u.slice(3);
-    
-    // 用空字串取代掉所有的 -
-    u = u.replace(/-/g, '');  // u = u.split('-').join('');
-
-    res.json({mobile: u});
-});
 
 app.use('/admin2',  require('./routes/admin2') );
-app.use('/address-book',  require('./routes/address-book') );
-
-app.get('/try-session', (req, res)=>{
-    req.session.my_var = req.session.my_var || 0;
-    req.session.my_var++;
-    res.json(req.session);
-});
-
-app.get('/try-moment', (req, res)=>{
-    const fm = 'YYYY-MM-DD HH:mm:ss';
-    res.json({
-        mo1: moment().format(fm),
-        mo2: moment().tz('Europe/London').format(fm),
-        mo3: moment(req.session.cookie.expires).format(fm),
-        mo4: moment(req.session.cookie.expires).tz('Europe/London').format(fm),
-    });
-});
-
-app.get('/try-db', async (req, res)=>{
-    const sql = "SELECT * FROM address_book LIMIT 5";
-
-    const [rs, fields] = await db.query(sql);
-
-    res.json(rs);
-
-});
-
-app.get('/yahoo', async (req, res)=>{
-
-    fetch('https://tw.yahoo.com/')
-        .then(r=>r.text())
-        .then(txt=>{
-            res.send(txt);
-        });
-
-});
-app.get('/yahoo2', async (req, res)=>{
-
-    const response = await axios.get('https://tw.yahoo.com/');
-    console.log(response);
-    res.send(response.data);
-
-});
-
-// 登入的表單
-app.get('/login', async (req, res)=>{
-    res.render('login');
-});
-// 檢查登入帳密
-app.post('/login', async (req, res)=>{
-
-    const output = {
-        success: false,
-        error: '',
-        info: null,
-        token: '',
-        code: 0,
-    };
-
-    const [rs] = await db.query('SELECT * FROM admins WHERE account=?', [req.body.account]);
-
-    if(! rs.length){
-        output.error = '帳密錯誤';
-        output.code = 401;
-        return res.json(output);
-    }
-    const row = rs[0];
-
-    const compareResult = await bcrypt.compare(req.body.password, row.password);
-    if(! compareResult){
-        output.error = '帳密錯誤';
-        output.code = 402;
-        return res.json(output);
-    }
-    
-    const {sid, account, avatar, nickname} = row;
-    output.success = true;
-    output.info = {account, avatar, nickname};
-
-    output.token = jwt.sign({sid, account}, process.env.JWT_KEY);
 
 
-    res.json(output);
 
-});
+
+
+
+
+
+
+
+
 
 
 // ********** 所有路由的後面
--- routes/admin2.js
+++ routes/admin2.js
@@ -3,70 +3,264 @@ const req = require('express/lib/request');
 const res = require('express/lib/response');
 const db = require('./../modules/connect-db');
 const upload = require('./../modules/upload-imgs');
+const axios = require('axios');
+const hmacSHA256 = require('crypto-js/hmac-sha256');
+const CryptoJS =require('crypto-js'); 
+
+const Base64 = require('crypto-js/enc-base64');
 
 const router = express.Router();
 
+
+
+// 環境變數
+const {
+    LINEPAY_CHANNEL_ID,
+    LINEPAY_RETURN_HOST,
+    LINEPAY_SITE,
+    LINEPAY_VERSION,
+    LINEPAY_CHANNEL_SECRET_KEY,
+    LINEPAY_RETURN_CONFIRM_URL,
+    LINEPAY_RETURN_CANCEL_URL,
+} = process.env;
+
+
+
 // 自訂的 middleware
-router.use((req, res, next)=>{
-    res.locals.shin += ' admin2';
-    next();
-});
+// router.use((req, res, next) => {
+//     res.locals.shin += ' admin2';
+//     next();
+// });
+let orders = {};
+router.post('/checkout', async (req, res) => {
+    let frontendData = req.body;
+    console.log('frontendData', frontendData);
+    let frontendDataDeleAttr = [];
+    const DeletAttr = () => {
+        for (let i = 0; i < frontendData.length; i++) {
+            console.log('for')
+            delete frontendData[i].incre;
+            delete frontendData[i].decre;
+            delete frontendData[i].total;
+            delete frontendData[i].key;
+        }
+        return frontendData;
 
+    }
+    DeletAttr();
+    console.log('afterDeleAttr', frontendData)
+    let package = []
 
-router.get('/myform/:sid', async (req, res)=>{
-    const sid = parseInt(req.params.sid) || 0;
-    const [rs] = await db.query(`SELECT account,avatar,nickname FROM admins WHERE sid=${sid}`);
+    const packageGenerate = () => {
+        for (let i = 0; i < frontendData.length; i++) {
+            package.push({
+                id: 'donuts',
+                amount: frontendData[i].price*frontendData[i].quantity,
+                products: [
+                    frontendData[i]
+                ]
+            })
+        }
+        return package;
+    }
+    packageGenerate();
+    console.log('afterpackageGenerate',package);
 
-    res.json(rs);
-});
-router.put('/myform/:sid', upload.single('avatar'), async (req, res)=>{
-    // return res.json(req.body);
-    let modifyAvatar = '';
-    if(req.file && req.file.filename){
-        modifyAvatar = ` , avatar='${req.file.filename}' `;
+    // after
+    // { name: '豆漿波堤', price: 40, id: 1, quantity: 1 },
+    // { name: '原味波堤', price: 35, id: 2, quantity: 1 }
+
+    let order = {
+        amount: frontendData.map((data) => data.price * 1 * data.quantity).reduce((a, b) => a + b),//package.amount 相加
+        currency: 'TWD',
+        orderId: parseInt(new Date().getTime() / 1000),
+        packages: package,
     };
+    console.log('order',order);
+    if(order){
+        //後端收到訂單打line api('/payments/request')
+        try {
+            console.log('try',order)
+            let linepayBody = {
+                ...order,
+                'redirectUrls':{
+                    confirmUrl:`${LINEPAY_RETURN_HOST}${LINEPAY_RETURN_CONFIRM_URL}`,
+                    cancelUrl:`${LINEPAY_RETURN_HOST}${LINEPAY_RETURN_CANCEL_URL}`,
+                },
+            };
+            console.log('linepayBody',linepayBody)
+            const uri = '/payments/request';
+            const nonce = parseInt(new Date().getTime() / 1000);
 
-    const sql = `UPDATE admins SET nickname=? ${modifyAvatar} WHERE sid=? `;
+            const signature = Base64.stringify(hmacSHA256(`${LINEPAY_CHANNEL_SECRET_KEY}/${LINEPAY_VERSION}${uri}${JSON.stringify(linepayBody)}${nonce}`,LINEPAY_CHANNEL_SECRET_KEY,));
+            console.log('signature',signature);
+            const headers = {
+                'X-LINE-ChannelId':LINEPAY_CHANNEL_ID,
+                'Content-Type':'application/json',
+                'X-LINE-Authorization-Nonce':nonce,
+                'X-LINE-Authorization':signature,
+            };
+            console.log('headers',headers);
+            const posturi = `${LINEPAY_SITE}/v3/payments/request`;
 
-    const result = await db.query(sql, [req.body.nickname, req.params.sid ]);
+    const linePayRes = await axios.post(posturi,linepayBody,{headers});
+    console.log('linePayRes',linePayRes);
+    console.log('linePayRes',linePayRes.data.info);
+    if(linePayRes?.data?.returnCode === '0000'){
+        res.json(linePayRes.data.info);
+    }
 
-    res.json(result);
+    } catch (error) {
+        console.log('error',error);
+        res.end();
+    }
 
-});
+    }else{
+        console.log('無訂單')
+        res.json({
+            'message':'訂單金額為零'
+        })
+    }
+    //order insert orderID to fit in line pay 資料結構
+    // order.orderId = parseInt(new Date().getTime() / 1000);
+    // orders[order.orderId]=order;
+    // orders {
+    //     '1678452076': {
+    //       amount: 1000,
+    //       currency: 'TWD',
+    //       packages: [ [Object] ],
+    //       orderID: 1678452076
+    //     }
+    //   }
+    //   checkout order {
+    //     amount: 1000,
+    //     currency: 'TWD',
+    //     packages: [ { id: 'products_1', amount: 1000, products: [Array] } ],
+    //     orderID: 1678452076
+    //   }
+    // res.json(order);
+    // console.log('orders',orders)
+    // console.log('checkout order', order);
+    // console.log('checkout orderid', order.orderId);
+    // res.end();
 
-router.get('/', (req, res)=>{
-    res.send('admin2: root');
-});
+})
 
-router.get('/abc', (req, res)=>{
-    res.json({
-        originalUrl: req.originalUrl,
-        'locals.shin': res.locals.shin
-    });
-});
-router.get('/def', (req, res)=>{
-    res.json({
-        originalUrl: req.originalUrl,
-        'locals.shin': res.locals.shin
-    });
-});
+router.post('/createOrder/:orderid', async (req, res) => {
+    const { orderid } = req.params;
+    let order = orders[orderid];
+    console.log('createOrder orderid', orderid)
+    console.log('createOrder orders', orders)
+    console.log('createOrder order', order)
+    // createOrder orders {
+    //     '1678452076': {
+    //       amount: 1000,
+    //       currency: 'TWD',
+    //       packages: [ [Object] ],
+    //       orderID: 1678452076
+    //     }
+    //   }
+    // createOrder order {
+    //     amount: 1000,
+    //     currency: 'TWD',
+    //     packages: [ { id: 'products_1', amount: 1000, products: [Array] } ],
+    //     orderID: 1678452076
+    //   }
 
-router.get('/:p1?/:p2?', (req, res)=>{
-    let {
-        params,
-        url,
-        originalUrl,
-        baseUrl,
-    } = req;
 
-    res.json({
-        params,
-        url,
-        originalUrl,
-        baseUrl,
-        'locals.shin': res.locals.shin
-    });
-});
+    // line api Signature 格式(from document)
+    // Signature = Base64(HMAC-SHA256(Your ChannelSecret, (Your ChannelSecret + URI + RequestBody + nonce)))
+    try {
+        let linepayBody = {
+            ...order,
+            'currency': 'TWD',
+            'redirectUrls': {
+                confirmUrl: `${LINEPAY_RETURN_HOST}${LINEPAY_RETURN_CONFIRM_URL}`,
+                cancelUrl: `${LINEPAY_RETURN_HOST}${LINEPAY_RETURN_CANCEL_URL}`,
+            },
+        };
+        console.log('linepayBody', linepayBody)
+        const uri = '/payments/request';
+        const nonce = parseInt(new Date().getTime() / 1000);
+
+        const signature = Base64.stringify(hmacSHA256(`${LINEPAY_CHANNEL_SECRET_KEY}/${LINEPAY_VERSION}${uri}${JSON.stringify(linepayBody)}${nonce}`, LINEPAY_CHANNEL_SECRET_KEY,));
+        const headers = {
+            'X-LINE-ChannelId': LINEPAY_CHANNEL_ID,
+            'Content-Type': 'application/json',
+            'X-LINE-Authorization-Nonce': nonce,
+            'X-LINE-Authorization': signature,
+        };
+        console.log('headers', headers);
+        const posturi = `${LINEPAY_SITE}/v3/payments/request`;
+        const linePayRes = await axios.post(posturi, linepayBody, { headers });
+        console.log('linePayRes', linePayRes.data.info);
+        if (linePayRes?.data?.returnCode === '0000') {
+            res.json(linePayRes.data.info);
+            // res.redirect(linePayRes?.data?.info?.web)
+        } else {
+            console.log('end')
+        };
+
+        // res.json(order);
+
+    } catch (error) {
+        console.log('error', error);
+        res.end();
+    }
+
+})
+
+router.get('/confirm', async (req, res) => {
+    const { transactionId, orderId } = req.query;
+    const order = orders[orderId];
+
+    try {
+        let linepayBody = {
+            "amount": order.amount,
+            'currency': 'TWD',
+        };
+        console.log('linepayBody', linepayBody)
+        // 文件 POST /v3/payments/{transactionId}/confirm
+        const uri = `/payments/${transactionId}/confirm`;
+        const nonce = parseInt(new Date().getTime() / 1000);
+
+        const signature = Base64.stringify(hmacSHA256(`${LINEPAY_CHANNEL_SECRET_KEY}/${LINEPAY_VERSION}${uri}${JSON.stringify(linepayBody)}${nonce}`, LINEPAY_CHANNEL_SECRET_KEY,));
+        const headers = {
+            'X-LINE-ChannelId': LINEPAY_CHANNEL_ID,
+            'Content-Type': 'application/json',
+            'X-LINE-Authorization-Nonce': nonce,
+            'X-LINE-Authorization': signature,
+        };
+        console.log('headers', headers);
+        const posturi = `${LINEPAY_SITE}/v3/payments/${transactionId}/confirm`;
+        const linePayRes = await axios.post(posturi, linepayBody, { headers });
+        console.log('linePayRes', linePayRes);
+        if (linePayRes?.data?.returnCode === '0000') {
+            // res.json(linePayRes.data.info);
+            res.redirect('http://localhost:3000/linePay/confirm')
+        } else {
+            console.log('end')
+        };
+
+        // res.json(order);
+
+    } catch (error) {
+        console.log('error', error);
+        res.end();
+    }
+
+
+
+})
+
+
+
+
+
+
+
+
+
 
 module.exports = router;
 
