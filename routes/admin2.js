80d42999a3074bb8eb8b37dcb647d3bfa0eea864
yuchen <wameny10814@gmail.com>
2023-04-11 17:36:43 +0800

    230411 客服ROUTER

--- routes/admin2.js
+++ routes/admin2.js
@@ -1,15 +1,14 @@
 const express = require('express');
 const req = require('express/lib/request');
 const res = require('express/lib/response');
-// const db = require('./../modules/connect-db');
-// const upload = require('./../modules/upload-imgs');
 const axios = require('axios');
 const hmacSHA256 = require('crypto-js/hmac-sha256');
-const CryptoJS =require('crypto-js'); 
+const CryptoJS = require('crypto-js');
 
 const Base64 = require('crypto-js/enc-base64');
 
 const router = express.Router();
+const nodemailer = require("nodemailer");
 
 
 
@@ -22,6 +21,9 @@ const {
     LINEPAY_CHANNEL_SECRET_KEY,
     LINEPAY_RETURN_CONFIRM_URL,
     LINEPAY_RETURN_CANCEL_URL,
+    CONTACT_MAIL,
+    CONTACT_MAIL_USER,
+    CONTACT_MAIL_PASS,
 } = process.env;
 
 
@@ -55,7 +57,7 @@ router.post('/checkout', async (req, res) => {
         for (let i = 0; i < frontendData.length; i++) {
             package.push({
                 id: 'donuts',
-                amount: frontendData[i].price*frontendData[i].quantity,
+                amount: frontendData[i].price * frontendData[i].quantity,
                 products: [
                     frontendData[i]
                 ]
@@ -64,7 +66,7 @@ router.post('/checkout', async (req, res) => {
         return package;
     }
     packageGenerate();
-    console.log('afterpackageGenerate',package);
+    console.log('afterpackageGenerate', package);
 
     // after
     // { name: '豆漿波堤', price: 40, id: 1, quantity: 1 },
@@ -76,49 +78,49 @@ router.post('/checkout', async (req, res) => {
         orderId: parseInt(new Date().getTime() / 1000),
         packages: package,
     };
-    console.log('order',order);
-    if(order){
+    console.log('order', order);
+    if (order) {
         //後端收到訂單打line api('/payments/request')
         try {
-            console.log('try',order)
+            console.log('try', order)
             let linepayBody = {
                 ...order,
-                'redirectUrls':{
-                    confirmUrl:`${LINEPAY_RETURN_HOST}${LINEPAY_RETURN_CONFIRM_URL}`,
-                    cancelUrl:`${LINEPAY_RETURN_HOST}${LINEPAY_RETURN_CANCEL_URL}`,
+                'redirectUrls': {
+                    confirmUrl: `${LINEPAY_RETURN_HOST}${LINEPAY_RETURN_CONFIRM_URL}`,
+                    cancelUrl: `${LINEPAY_RETURN_HOST}${LINEPAY_RETURN_CANCEL_URL}`,
                 },
             };
-            console.log('linepayBody',linepayBody)
+            console.log('linepayBody', linepayBody)
             const uri = '/payments/request';
             const nonce = parseInt(new Date().getTime() / 1000);
 
-            const signature = Base64.stringify(hmacSHA256(`${LINEPAY_CHANNEL_SECRET_KEY}/${LINEPAY_VERSION}${uri}${JSON.stringify(linepayBody)}${nonce}`,LINEPAY_CHANNEL_SECRET_KEY,));
-            console.log('signature',signature);
+            const signature = Base64.stringify(hmacSHA256(`${LINEPAY_CHANNEL_SECRET_KEY}/${LINEPAY_VERSION}${uri}${JSON.stringify(linepayBody)}${nonce}`, LINEPAY_CHANNEL_SECRET_KEY,));
+            console.log('signature', signature);
             const headers = {
-                'X-LINE-ChannelId':LINEPAY_CHANNEL_ID,
-                'Content-Type':'application/json',
-                'X-LINE-Authorization-Nonce':nonce,
-                'X-LINE-Authorization':signature,
+                'X-LINE-ChannelId': LINEPAY_CHANNEL_ID,
+                'Content-Type': 'application/json',
+                'X-LINE-Authorization-Nonce': nonce,
+                'X-LINE-Authorization': signature,
             };
-            console.log('headers',headers);
+            console.log('headers', headers);
             const posturi = `${LINEPAY_SITE}/v3/payments/request`;
 
-    const linePayRes = await axios.post(posturi,linepayBody,{headers});
-    console.log('linePayRes',linePayRes);
-    console.log('linePayRes',linePayRes.data.info);
-    if(linePayRes?.data?.returnCode === '0000'){
-        res.json(linePayRes.data.info);
-    }
+            const linePayRes = await axios.post(posturi, linepayBody, { headers });
+            console.log('linePayRes', linePayRes);
+            console.log('linePayRes', linePayRes.data.info);
+            if (linePayRes?.data?.returnCode === '0000') {
+                res.json(linePayRes.data.info);
+            }
 
-    } catch (error) {
-        console.log('error',error);
-        res.end();
-    }
+        } catch (error) {
+            console.log('error', error);
+            res.end();
+        }
 
-    }else{
+    } else {
         console.log('無訂單')
         res.json({
-            'message':'訂單金額為零'
+            'message': '訂單金額為零'
         })
     }
     //order insert orderID to fit in line pay 資料結構
@@ -149,9 +151,9 @@ router.post('/checkout', async (req, res) => {
 router.post('/createOrder/:orderid', async (req, res) => {
     const { orderid } = req.params;
     let order = orders[orderid];
-    console.log('createOrder orderid', orderid)
-    console.log('createOrder orders', orders)
-    console.log('createOrder order', order)
+    // console.log('createOrder orderid', orderid)
+    // console.log('createOrder orders', orders)
+    // console.log('createOrder order', order)
     // createOrder orders {
     //     '1678452076': {
     //       amount: 1000,
@@ -252,6 +254,52 @@ router.get('/confirm', async (req, res) => {
 
 
 })
+
+router.post('/contactus', async (req, res) => {
+    const { name, email, context } = req.body;
+    const output = {
+        success: false,
+    };
+    const transporter = nodemailer.createTransport({
+        host: "smtp.gmail.com",
+        port: 465,
+        auth: {
+            user: CONTACT_MAIL_USER,
+            pass: CONTACT_MAIL_PASS,
+        },
+    });
+    //信件內容!!!!
+    var options = {
+        //寄件者
+        from: email,
+        //收件者
+        to: CONTACT_MAIL,
+        //副本
+        // cc: 'account3@gmail.com',
+        //密件副本
+        // bcc: 'account4@gmail.com',
+        //主旨
+        subject: "來自" + name + "客服訊息", // Subject line
+        //純文字
+        text: " ", // plaintext body
+        //嵌入 html 的內文
+        html: `<p>${context}</p>`,
+        //附件檔案
+    };
+
+    // 信件發送!!!
+    transporter.sendMail(options, function (error, info) {
+        if (error) {
+            console.log(error);
+        } else {
+            console.log("訊息發送: " + info.response);
+        }
+    });
+    const result = { ...output, success: true };
+    res.json(result);
+
+})
+
 
 
 
